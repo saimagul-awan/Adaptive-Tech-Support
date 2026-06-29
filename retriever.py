22f0b80a80d929d7a8647205dc7fa083908051d2
saimagul-awan <saimagul.awan@gmail.com>
2026-06-29 14:55:41 +0500

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
